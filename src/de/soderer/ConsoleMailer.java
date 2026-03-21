3fecc66f61f90bd5f8e41d8de5fd1ebc48871f5b
Andreas Soderer <git@soderer.de>
2026-03-21 20:49:07 +0100

    Restructured project

--- src/de/soderer/ConsoleMailer.java
+++
@@ -1,9 +0,0 @@
-package de.soderer;
-
-import de.soderer.mailer.utilities.jarinjarloader.JarInJarLoader;
-
-public class ConsoleMailer extends JarInJarLoader {
-	public static void main(final String[] args) throws Exception {
-		jarInJarLoaderStart(args);
-	}
-}
---
+++ src/main/java/de/soderer/ConsoleMailer.java
@@ -0,0 +1,9 @@
+package de.soderer;
+
+import de.soderer.mailer.utilities.jarinjarloader.JarInJarLoader;
+
+public class ConsoleMailer extends JarInJarLoader {
+	public static void main(final String[] args) throws Exception {
+		jarInJarLoaderStart(args);
+	}
+}
